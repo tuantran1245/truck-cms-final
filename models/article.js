f1ee8cd2f7fb89885fa3ab821a6f9e36f648dbf5
Tuantq <tuantq@imosys.vn>
2018-10-19 05:02:15 +0700

    hoan thien MVC

--- models/article.js
+++ models/article.js
@@ -1,53 +1,53 @@
-// module.exports = (sequelize, DataTypes) => {
-//     var Article = sequelize.define('vehicle', {
-//         id: {
-//             type: DataTypes.INTEGER,
-//             primaryKey: true,
-//             autoIncrement: true
-//         },
-//         user_id: {
-//             type: DataTypes.INTEGER,
-//             allowNull: false,
-//             // references: {
-//             //     model: User,
-//             //     key: 'id',
-//             // }
-//         },
-//         vehicle_id: {
-//             type: DataTypes.INTEGER,
-//             allowNull: false,
-//             // references: {
-//             //     model: Vehicle,
-//             //     key: 'id',
-//             // }
-//         },
-//         title: {
-//             type: DataTypes.STRING,
-//             allowNull: false
-//         },
-//         content: {
-//             type: DataTypes.TEXT,
-//             allowNull: false
-//         }
-//     }, {
-//             timestamps: true,
-//             tableName: 'article',
-//             underscored: true,
-//         });
+module.exports = (sequelize, DataTypes) => {
+    var Article = sequelize.define('Article', {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            // references: {
+            //     model: User,
+            //     key: 'id',
+            // }
+        },
+        vehicle_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            // references: {
+            //     model: Vehicle,
+            //     key: 'id',
+            // }
+        },
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        content: {
+            type: DataTypes.TEXT,
+            allowNull: false
+        }
+    }, {
+            timestamps: true,
+            tableName: 'article',
+            underscored: true,
+        });
 
-//     Article.associate = function (models) {
-//         Article.belongsTo(models.Vehicle, {
-//             foreignKey: 'vehicle_id',
-//             as: 'vehicle'
-//         });
-//         Article.belongsTo(models.User, {
-//             foreignKey: 'user_id',
-//             as: 'user'
-//         });
-//         Article.hasMany(models.Image, {
-//             foreignKey: 'article_id',
-//             as: 'image'
-//         });    
-//     };
-//     return Article;
-// };
+    Article.associate = function (models) {
+        Article.belongsTo(models.Vehicle, {
+            foreignKey: 'vehicle_id',
+            as: 'vehicle'
+        });
+        Article.belongsTo(models.User, {
+            foreignKey: 'user_id',
+            as: 'user'
+        });
+        Article.hasMany(models.Image, {
+            foreignKey: 'article_id',
+            as: 'image'
+        });    
+    };
+    return Article;
+};
--- models/user.js
+++ models/user.js
@@ -67,43 +67,52 @@
 // };
 
 'use strict';
-module.exports = function(sequelize, DataTypes) {
-  var User = sequelize.define('User', {
-    first_name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-       validate: {
-        len: [2,10]
-       }
-    },
-    last_name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-       validate: {
-        len: [2,10]
-       }
-    },
-    email: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique: true,
-       validate: {
-        isEmail: true
-       }
-    },
-    date_register: {
-      type: DataTypes.STRING,
-      defaultValue: sequelize.fn('NOW')
-    },
-    photo: {
-      type: DataTypes.STRING
-    }
-  }, {
-    classMethods: {
-      associate: function(models) {
-        // associations can be defined here
-      }
-    }
-  });
-  return User;
+module.exports = function (sequelize, DataTypes) {
+    var User = sequelize.define('User', {
+        first_name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [2, 10]
+            }
+        },
+        last_name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [2, 10]
+            }
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+            validate: {
+                isEmail: true
+            }
+        },
+        date_register: {
+            type: DataTypes.STRING,
+            defaultValue: sequelize.fn('NOW')
+        },
+        photo: {
+            type: DataTypes.STRING
+        }
+    }, {
+            classMethods: {
+                associate: function (models) {
+                    User.hasMany(models.Vehicle, {
+                        foreignKey: 'user_id',
+                        as: 'vehicle'
+                    });
+
+                    User.hasMany(models.Article, {
+                        foreignKey: 'user_id',
+                        as: 'vehicle'
+                    });
+
+                }
+            }
+        });
+    return User;
 };
