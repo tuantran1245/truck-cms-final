68e4d4be5117bc449b1cdaca8ef189ebbc081f0d
Tuantq <tuantq@imosys.vn>
2018-10-20 16:28:39 +0700

    cap nhat CRUD

--- controllers/user.js
+++ controllers/user.js
@@ -1,69 +1,51 @@
 const User = require('../models').User;
 
 module.exports = {
-	list: async (req, res) => {
-	  return User
-		.findAll()
-		.then((users) => {
-			res.render('user/index', {users: users});
-		})
-		.catch((error) => { res.render('error', error); });
-	},
-  
-	getById: async (req, res) => {
-	  return User
-		.findById(req.params.id)
-		.then((user) => {
-		  if (!user) {
-			return res.status(404).send({
-			  message: 'User Not Found',
-			});
-		  }
-		  res.render('user/show', user.dataValues)
-		})
-		.catch((error) => res.render('error', error));
-	},
-  
-	add: async (req, res) => {
-	  return User
-		.create({
-		  full_name: req.body.full_name,
-		  email: req.body.email,
-		  dob: req.body.dob,
-		  phone: req.body.phone,
-		  address: req.body.address,
-		  identity_number: req.body.identity_number,
-		  license_number: req.body.license_number
-		})
-		.then((user) => res.redirect('/users'))
-		.catch((error) => res.render('error', error));
-	},
-
-	edit: async (req, res) => {
+	list: (req, res) => {
 		return User
-		.findById(req.params.id)
-		.then((user) => {
-		  if (!user) {
-			return res.status(404).send({
-			  message: 'User Not Found',
-			});
-		  }
-		  res.render('user/edit', user.dataValues)
-		})
-		.catch((error) => res.render('error', error));
+			.findAll()
+			.then((users) => {
+				res.render('user/index', { users: users });
+			})
+			.catch((error) => { res.render('error', error); });
 	},
 
-	update: async(req, res) => {
-	  return User
-		.findById(req.params.id)
-		.then(user => {
-		  if (!user) {
-			return res.status(404).send({
-			  message: 'User Not Found',
-			});
-		  }
-		  return user
-			.update({
+	// list: () => {
+	// 	return User.findAll().then(users => {
+	// 		return users;
+	// 	}).catch((error) => {
+	// 		console.log(JSON.stringify(error, undefined, 2));
+	// 		return error;
+	// 	});
+	// },
+
+	getById: (req, res) => {
+		return User
+			.findById(req.params.id)
+			.then((user) => {
+				if (!user) {
+					return res.status(404).send({
+						message: 'User Not Found',
+					});
+				}
+				res.render('user/show', { user: user })
+			})
+			.catch((error) => res.render('error', error));
+	},
+
+	// getById: (id) => {
+	// 	return User.findById(id).then(user => {
+	// 		console.log(JSON.stringify(user, undefined, 2));
+	// 		return user;
+	// 	}).catch((error) => {
+	// 		console.log(JSON.stringify(error, undefined, 2));
+	// 		return error;
+	// 	});
+	// },
+
+	add: (req, res) => {
+		return User
+			.create({
 				full_name: req.body.full_name,
 				email: req.body.email,
 				dob: req.body.dob,
@@ -72,31 +54,141 @@ module.exports = {
 				identity_number: req.body.identity_number,
 				license_number: req.body.license_number
 			})
-			.then(() => res.status(200).send(user))
+			.then((user) => res.redirect('/users'))
 			.catch((error) => res.render('error', error));
-		})
-		.catch((error) => res.render('error', error));
 	},
-  
-	delete: async (req, res) {
-	  return User
-		.findById(req.params.id)
-		.then(user => {
-		  if (!user) {
-			return res.status(400).send({
-			  message: 'User Not Found',
-			});
-		  }
-		  return user
-			.destroy()
-			.then(() => res.status(204).send())
-			.catch((error) => res.render('error', error));
-		})
-		.catch((error) => res.render('error', error));
-	},
-  };
-  
+	//
+	// add: (full_name, email, dob, phone, address, identity_number, license_number, res) => {
+	// 	return User.create({
+	// 	  full_name: full_name,
+	// 	  email: email,
+	// 	  dob: dob,
+	// 	  phone: phone,
+	// 	  address: address,
+	// 	  identity_number: identity_number,
+	// 	  license_number: license_number
+	// 	}).then( () => {
+	// 		res.redirect('/users')
+	// 	}).catch((error) => {
+	// 		console.log(JSON.stringify(error, undefined, 2));
+	// 		return error;
+	// 	});
+	// 	},
 
+	edit: (req, res) => {
+		return User
+			.findById(req.params.id)
+			.then((user) => {
+				if (!user) {
+					return res.status(404).send({
+						message: 'User Not Found',
+					});
+				}
+				res.render('user/edit', { user: user })
+			})
+			.catch((error) => res.render('error', error));
+	},
+
+	// edit: (id) => {
+	// 	return User.findById(id).then(user => {
+	// 		return user;
+	// 	}).catch((error) => {
+	// 		return error;
+	// 	});
+	// },
+
+	update: async (req, res) => {
+		return User
+			.findById(req.params.id)
+			.then(user => {
+				if (!user) {
+					return res.status(404).send({
+						message: 'User Not Found',
+					});
+				}
+				return user
+					.update({
+						full_name: req.body.full_name,
+						email: req.body.email,
+						dob: req.body.dob,
+						phone: req.body.phone,
+						address: req.body.address,
+						identity_number: req.body.identity_number,
+						license_number: req.body.license_number
+					})
+					.then(() => res.status(200).send(user))
+					.catch((error) => res.render('error', error));
+			})
+			.catch((error) => res.render('error', error));
+	},
+
+	// update: (id,full_name, email, dob, phone, address, identity_number, license_number) => {
+	// 	return User.findById(id).then(user => {
+	// 		return user.update({
+	// 			full_name: full_name,
+	// 			email: email,
+	// 			dob: dob,
+	// 			phone: phone,
+	// 			address: address,
+	// 			identity_number:identity_number,
+	// 			license_number: license_number
+	// 		});
+	// 	}).catch((error) => {
+	// 		return error;
+	// 	});
+	// },
+
+	delete: (req, res) => {
+		User.findById(req.params.id)
+			.then(user => {
+				if (!user) {
+					return res.status(400).send({
+						message: 'User Not Found',
+					});
+				}
+				User.destroy({
+					where: {
+						id: user.id
+					}
+				}).then(() => {
+					res.redirect('/users')
+				}).catch((err) => {
+					res.render('error', err)
+				})
+			})
+			.catch((error) => res.render('error', error));
+	},
+};
+
+// router.get('/:id/delete', (req, res) => {
+// 	User.findById(req.params.id)
+// 		.then((user) => {
+// 			console.log(user.dataValues.photo)
+// 			fs.unlink(`uploads/photo/${user.dataValues.photo}`, () => {
+// 				User.destroy({
+// 					where: {
+// 						id: user.dataValues.id
+// 					}
+// 				}).then(() => {
+// 					res.redirect('/users')
+// 				}).catch((err) => {
+// 					res.render('error', err)
+// 				})
+// 			})
+// 		}).catch((err) => {
+// 			res.render('error', err)
+// 		})
+// })
+
+
+// 	delete: (id) => {
+// 		return User.findById(id).then(user =>{
+// 			return user.destroy()
+// 		}).catch((error) => {
+// 				return error;
+// 		});
+// 	},
+// }
 
 // var express = require('express');
 // var fs = require('fs');
@@ -135,7 +227,7 @@ module.exports = {
 // 		full_name: req.body.full_name,
 // 		email: req.body.email,
 // 		dob: req.body.dob,
-		
+
 // 		//photo: !req.file ? 'placeholder.jpg' : req.file.filename
 // 	}).then((user) => {
 // 		res.redirect('/users')
--- routes/users.js
+++ routes/users.js
@@ -1,14 +1,43 @@
 var express = require('express');
 var router = express.Router();
-
-const userController = require('../controllers').user;
+const userController = require('../controllers/user');
 
 /* user Router */
 router.get('/', userController.list);
-router.get('/new', userController.add);
-router.get('/:id/list', userController.getById);
+router.post('/', userController.add);
+router.get('/:id/detail', userController.getById);
 router.get('/:id/edit', userController.edit);
-router.put('/:id/edit/', userController.update);
-router.delete('/:id/delete', userController.delete);
+router.put('/:id/update/', userController.update);
+router.get('/:id/delete', userController.delete);
+
+router.get('/new', (req, res) => {
+    res.render('user/create');
+});
+
+// router.get('/', async (req, res) => {
+//     res.render('user/index', {
+//         users: await userController.list()
+//     });
+// });
+
+// router.get('/:id/detail', async (req, res) => {
+//     res.render('user/show', {
+//         user: await userController.add(req.params.id)
+//     });
+// });
+
+// router.get('/new', async (req, res) => {
+//     res.render('user/create');
+// });
+
+// router.post('/', async (req, res) => {
+//     userController.add(req.body.full_name, req.body.email, req.body.dob, req.body.phone, req.body.address, req.body.identity_number, req.body.license_number, res)
+// });
+
+// router.get('/:id/edit', async (req, res) => {
+//     res.render('user/edit', {
+//         user: await userController.edit(req.params.id)
+//     });
+// });
 
 module.exports = router;
