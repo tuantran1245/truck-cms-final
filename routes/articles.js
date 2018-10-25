87b0cb875b0b61a56ce63e1bf92732e9bd6e0cee
Tuantq <tuantq@imosys.vn>
2018-10-26 00:46:00 +0700

    be positive

--- routes/articles.js
+++ routes/articles.js
@@ -6,9 +6,9 @@ const articleController = require('../controllers/article');
 router.get('/', articleController.list);
 router.post('/', articleController.add);
 router.get('/:id/detail', articleController.getById);
-// router.get('/:id/edit', articleController.edit);
-// router.put('/:id/update/', articleController.update);
-// router.get('/:id/delete', articleController.delete);
+router.get('/:id/edit', articleController.edit);
+router.put('/:id/update/', articleController.update);
+router.get('/:id/delete', articleController.delete);
 
 router.get('/new', (req, res) => {
     res.render('article/create');
---
+++ views/utils.js
@@ -0,0 +1,10 @@
+module.exports = {
+    isUndefined(data) {
+        if (typeof (data) == 'undefined') {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+}
